04fa979d8267271c620ce3977783eb07fd197559
FKingQ <luxborj@gmail.com>
2023-03-06 23:28:53 +0800

    Update 3/6/2023 11:28pm

--- resources/views/services/map.blade.php
+++ resources/views/services/map.blade.php
@@ -9,57 +9,21 @@
     <iframe src="https://silentnatexd.users.earthengine.app/view/coaster" title="W3Schools Free Online Web Tutorials"  width="100%" height="980" style="border:none;"></iframe>
 </div> -->  
 
-<div id="googleMap" style="width:500px;height:380px;"></div>
+<div id="map" class="h-screen"></div>
 
-
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-    
-
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwgdsERhbvorxr1v2m33AqMyzvYhRRk0k&callback=initMap"
+    async defer></script>
 @endsection
 
 @section('js')
-
-<script>
-    var myCenter = new google.maps.LatLng(51.508742, -0.120850);
-
-function initialize() {
-    var mapProp = {
-        center: myCenter,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    var marker = new google.maps.Marker({
-        position: myCenter,
-    });
-    marker.setMap(map);
-    
-    google.maps.event.addListener(marker, 'click', function () {
-
-                $('#myModal').modal('show'); 
-            });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
 
 @endsection
