7cc6568c61b06cd54c0b60edcd63b8d1fbe6efbe
FKingQ <luxborj@gmail.com>
2023-03-13 23:23:48 +0800

    3/13/2023 11:23pm

--- resources/views/services/map.blade.php
+++ resources/views/services/map.blade.php
@@ -59,7 +59,7 @@
   function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 8,
-      mapTypeId: 'satellite',
+      mapTypeId: 'hybrid',
       center: { lat: 16.549020362670005, lng: 121.17665325405989},
       mapTypeControl: true,
       mapTypeControlOptions: {
@@ -81,6 +81,42 @@
       strictBounds: false,
     },
     });
+
+    var region1 = @json($region1);
+    console.log(region1);
+
+
+    const contentString = [];
+    for (let i = 0; i < region1.length; i++) {
+
+      contentString[i] ='<div class="w-1/2">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+          '<h1 id="firstHeading" class="firstHeading">' +
+          region1[i].name
+          '</h1>' +
+      "</div>";
+
+    }
+    for (let i = 0; i < region1.length; i++) {
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString[i],
+        ariaLabel: "Sample",
+      });
+
+      const marker = new google.maps.Marker({
+        position: { lat: region1[i].latitude, lng: region1[i].longitude},
+        map,
+        title: "Uluru (Ayers Rock)",
+      });
+
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    }
+  
+
   }
 
   window.initMap = initMap;
@@ -88,54 +124,66 @@
 </script>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqrwTHtdIIy2XFwP3GkVGaMqtEfjjwJuY&callback=initMap" type="text/javascript"></script>
 
+  <!-- Delete Article Modal -->
+  <div x-data="{ modalDelete: false }" class="grid grid-cols-1 gap-4 place-content-center">
+    <button @click="modalDelete =!modalDelete" class="block flex justify-center pr-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 stroke-rose-700 transition ease-in-out hover:-translate-y-1 hover:scale-125  duration-500">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+        </svg>
+    </button>
 
-	<div id="main-modal" class="main-modal fixed w-full inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster" style="background: rgba(0,0,0,.7);">
-		<div class="border border-blue-500 shadow-lg modal-container bg-white w-4/12 md:max-w-11/12 mx-auto rounded-xl shadow-lg z-50 overflow-y-auto">
-			<div class="modal-content py-4 text-left px-6">
-				<!--Title-->
-				<div class="flex justify-between items-center pb-3">
-					<p class="text-2xl font-bold text-gray-500">PROVICE</p>
-					<div class="modal-close cursor-pointer z-50" onclick="closeModal()">
-						<svg class="fill-current text-gray-500" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-							viewBox="0 0 18 18">
-							<path
-								d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
-							</path>
-						</svg>
-					</div>
-				</div>
-				<!--Body-->
-				<div class="my-5 mr-5 ml-5 flex justify-center">
-                    <form action="" method="POST" id="add_caretaker_form"  class="w-full">
-                    </form>
-				</div>
-				<!--Footer-->
-				<div class="flex justify-end pt-2 space-x-14">
-					<!-- <button class="px-4 bg-blue-500 p-3 ml-3 rounded-lg text-white hover:bg-teal-400" >Exit</button> -->
-				</div>
-			</div>
-		</div>
-	</div>
+    <div x-show="modalDelete" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+            <div x-cloak @click="modalDelete = false" x-show="modalDelete" 
+                x-transition:enter="transition ease-out duration-300 transform"
+                x-transition:enter-start="opacity-0" 
+                x-transition:enter-end="opacity-100"
+                x-transition:leave="transition ease-in duration-200 transform"
+                x-transition:leave-start="opacity-100" 
+                x-transition:leave-end="opacity-0"
+                class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"
+            ></div>
 
-<script>
+            <div x-cloak x-show="modalDelete" 
+                x-transition:enter="transition ease-out duration-300 transform"
+                x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" 
+                x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                x-transition:leave="transition ease-in duration-200 transform"
+                x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" 
+                x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl"
+            >
 
-    all_modals = ['main-modal']
-    all_modals.forEach((modal)=>{
-        const modalSelected = document.querySelector('.'+modal);
-        modalSelected.classList.remove('fadeIn');
-        modalSelected.classList.add('fadeOut');
-        modalSelected.style.display = 'none';
-    })
+                <!-- Delete Article -->
+                <form action="" method="POST">
+                    @csrf
+                        <!--body-->
+                        <div class="text-center p-5 flex-auto justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 flex items-center text-red-500 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                </svg>
+                                        <h2 class="text-xl font-bold py-4 ">Are you sure?</h3>
+                                        <p class="text-sm text-gray-500 px-8">Do you really want to delete this article?
+                                This process cannot be undone</p>    
+                        </div>
+                        <!--footer-->
+                        <div class="p-3  mt-2 text-center space-x-4 md:block">
+                            <button @click="modalDelete = false" type="button" class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+                                Cancel
+                            </button>
+                            <button class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Delete</button>
+                        </div>
+                </form>
+                <!-- Delete Article -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Delete Article Modal -->
 
-    function closeModal(){
-        const modalToClose =  document.getElementById("main-modal")
-        modalToClose.classList.remove('fadeIn');
-        modalToClose.classList.add('fadeOut');
-        setTimeout(() => {
-            modalToClose.style.display = 'none';
-        }, 500);
-    }
-</script> 
 
 @endsection
 
