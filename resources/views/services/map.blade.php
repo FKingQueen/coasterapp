e57b398fb987bbc96318da8302a6fd91f2fb31bb
Your Name <luxborj@gmail.com>
2023-03-16 14:15:01 +0800

    16/03/2023 2:14pm

--- resources/views/services/map.blade.php
+++ resources/views/services/map.blade.php
@@ -10,45 +10,8 @@
 </div>   -->
 <div id="map" class="h-screen"></div>
 <script>
-  // var region1 = @json($region1);
-
-  // var map;
-  // function initMap() {
-  //   map = new google.maps.Map(document.getElementById('map'), {
-  //     center: {lat: 16.549020362670005, lng: 121.17665325405989},
-  //     mapTypeId: 'satellite',
-  //     zoom: 8,
-  //     mapTypeId: google.maps.MapTypeId.HYBRID,
-  //   });
-
-  //   for (let i = 0; i < region1.length; i++) {
-      
-  //   var marker = new google.maps.Marker({
-  //   position: {lat: region1[i].latitude, lng: region1[i].longitude},
-  //   map: map,
-  //   animation:google.maps.Animation.BOUNCE,
-  //   title: region1[i].name,
-    
-  //   });
-
   
-  //   /*Google Map Marker Click Function*/
-    
-  //   google.maps.event.addListener(marker, 'click', (function(marker) {
-  //     return function() {
-  //       const modalToOpen =  document.getElementById("main-modal")
-  //       modalToOpen.classList.remove('fadeOut');
-  //       modalToOpen.classList.add('fadeIn');
-  //       modalToOpen.style.display = 'flex';
-  //       map.setZoom(9);
-  //     }
-  //   })(marker));
-    
-  //   }
-
-    
-  // }
-  const PHILIPPINES= {
+  const LUZON= {
     north: 18.96935395317229,
     south: 13.848818938737832,
     west: 114.41419433784404,
@@ -72,37 +35,20 @@
       },
       fullscreenControl: true,
       restriction: {
-      latLngBounds: PHILIPPINES,
+      latLngBounds: LUZON,
       strictBounds: false,
     },
     });
 
     var region1 = @json($region1);
 
-    // for (let i = 0; i < region1.length; i++) {
-    //   const cityCircle = new google.maps.Circle({
-    //     strokeColor: "#FF0000",
-    //     strokeOpacity: 0.8,
-    //     strokeWeight: 2,
-    //     fillColor: "#FF0000",
-    //     fillOpacity: 0.35,
-    //     map,
-    //     center: { lat: region1[i].latitude, lng: region1[i].longitude},
-    //     radius: 39928.215253,
-    //   });
-    // }
-
-  //   const svgMarker = {
-  //   path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-  //   fillColor: "blue",
-  //   fillOpacity: 0.6,
-  //   strokeWeight: 0,
-  //   rotation: 0,
-  //   scale: 2,
-  //   anchor: new google.maps.Point(0, 20),
-  // };
-    // Create an info window to share between markers.
     const infoWindow = new google.maps.InfoWindow();
+
+    var provices = document.getElementById("provice");
+
+    const card = document.getElementById("pac-card");
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(card);
+    card.style.display ="none";
 
     for (let i = 0; i < region1.length; i++) {
       const marker = new google.maps.Marker({
@@ -114,6 +60,7 @@
         optimized: false,
       });
 
+      marker.myData = i;
 
       const markerCenter = new google.maps.Marker({
         position: { lat: 16.545563, lng: 123.424705},
@@ -123,35 +70,98 @@
       });
 
       marker.addListener("click", () => {
-        // infoWindow.close();
-        // infoWindow.setContent(marker.getTitle());
-        // infoWindow.open(marker.getMap(), marker);
         map.panTo(markerCenter.getPosition());
-        // map.setCenter(markerCenter.getPosition());
-        // map.setCenter({ lat: 17.104225, lng: 124.229317});
+
+        // show card
+        card.style.display ="block";
+
+        console.log('Marker ' + marker.myData + ' has been clicked');
+        console.log('Value: ' + provices.value );
+
       });
 
-
-
-      // map.addListener("center_changed", () => {
-      //   // 3 seconds after the center of the map has changed, pan back to the
-      //   // marker.
-      //   window.setTimeout(() => {
-      //     map.panTo(markerCenter.getPosition());
-      //   }, 3000);
-      // });
+      map.addListener("drag", () => {
+        // hide card
+        card.style.display ="none";
+      });
 
     }
 
-  }
+    }
 
   window.initMap = initMap;
   
 </script>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqrwTHtdIIy2XFwP3GkVGaMqtEfjjwJuY&callback=initMap" type="text/javascript"></script>
 
-<!-- /Delete Article Modal -->
+<style>
 
+.pac-card {
+  background-color: #fff;
+  border: 0;
+  width: 700px;
+  border-radius: 2px;
+  box-shadow: 0 1px 4px -1px rgba(0, 0, 0, 0.3);
+  margin-right: 250px;
+  padding: 0 0.5em;
+  font: 400 18px Roboto, Arial, sans-serif;
+  overflow: hidden;
+  font-family: Roboto;
+  padding: 0;
+}
+
+#pac-container {
+  padding-bottom: 12px;
+  margin-right: 12px;
+}
+
+.pac-controls {
+  display: inline-block;
+  padding: 5px 11px;
+}
+
+.pac-controls label {
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 300;
+}
+
+#pac-input {
+  background-color: #fff;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 400px;
+}
+
+#pac-input:focus {
+  border-color: #4d90fe;
+}
+
+#title {
+  color: #fff;
+  background-color: #4d90fe;
+  font-size: 25px;
+  font-weight: 500;
+  padding: 6px 12px;
+}
+</style>
+<div class="pac-card" id="pac-card">
+      <div>
+        <div id="title" class="text-center">Height of Water Level During Typhoon</div>
+        <div id="type-selector" class="pac-controls w-full">
+          <select id="provice" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:border-blue-100 block w-full p-2.5">
+              <option selected>Choose a Typhoon</option>
+              <option value="US">Typhoon 1</option>
+              <option value="CA">Typhoon 2</option>
+              <option value="FR">Typhoon 3</option>
+              <option value="DE">Typhoon 4  </option>
+          </select>
+      </div>
+    </div>
 
 @endsection
 
